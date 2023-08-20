aaaa235804a4a55689de11a52cda40edaee46a55
Adie <capilitanadie@gmail.com>
2023-08-21 03:52:50 +0800

    Alatiris

---
+++ src/components/Business.jsx
@@ -0,0 +1,16 @@
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const Business = () => {
+  return (
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Why ATIS GATEWAY?</h2>
+        <p></p>
+      </div>
+    </section>
+  );
+};
+
+export default Business;
---
+++ src/constants/index.js
@@ -0,0 +1,226 @@
+import {
+  people01,
+  people02,
+  people03,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  airbnb,
+  binance,
+  coinbase,
+  dropbox,
+  send,
+  shield,
+  wallet,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: wallet,
+    title: "One business card is enough.",
+    content:
+      "Less is more when it comes to networking. A well-designed business card can make a lasting impression and open doors to endless opportunities. One card, endless possibilities.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Data Protection",
+    content:
+      "Data protection is not just a legal requirement, it’s a responsibility we all share. Safeguarding sensitive information ensures privacy, builds trust, and secures a better future for all.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Best UV printer printing quality",
+    content:
+      "When it comes to UV printers, the best printing quality is a combination of advanced technology, high-resolution printing heads, precise color management, and premium UV ink. Invest in quality for outstanding results.",
+  },
+  {
+    id: "Environmentally friendly",
+    icon: send,
+    title: "Balance Transfer",
+    content:
+      "Protecting the environment is crucial for our planet’s sustainability. Choosing eco-friendly options, reducing waste, and promoting sustainable practices are our responsibilities towards a better world for future generations.",
+  },
+  {
+    id: "feature-5",
+    icon: send,
+    title: "Lifetime usage of the platform",
+    content:
+      "The lifetime usage of a platform depends on its versatility, scalability, and adaptability to changing needs. A robust platform that evolves with technology and offers continuous support ensures long-term success for businesses and their customers",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+    name: "Herman Jensen",
+    title: "Founder & Leader",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Money makes your life easier. If you're lucky to have it, you're lucky.",
+    name: "Steve Mark",
+    title: "Founder & Leader",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "It is usually people in the money business, finance, and international trade that are really rich.",
+    name: "Kenn Gallagher",
+    title: "Founder & Leader",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "User Active",
+    value: "3800+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Company",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Transaction",
+    value: "$230M+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
